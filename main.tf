0acacc6fb755e2dc3cc242747def2ef5b170fe99
joe@joe.com <Joe>
2021-01-29 13:50:26 -0600

    server

--- main.tf
+++ main.tf
@@ -77,43 +77,43 @@ resource "azurerm_network_interface" "int1" {
   
 }
 
-#resource "azurerm_windows_virtual_machine" "vm1" {
-#  name                = "${var.companypf}-DC01"
-#  resource_group_name = azurerm_resource_group.rg1.name
-#  location            = azurerm_resource_group.rg1.location
-#  size                = "Standard_F2"
-#  admin_username      = var.admin_user
-#  admin_password      = var.admin_pass
-#  network_interface_ids = [
-#    azurerm_network_interface.int1.id,
-#  ]
-#    tags = {
-#    environment = "prod"
-#  }
+resource "azurerm_windows_virtual_machine" "vm1" {
+  name                = "${var.companypf}-DC01"
+  resource_group_name = azurerm_resource_group.rg1.name
+  location            = azurerm_resource_group.rg1.location
+  size                = "Standard_F2"
+  admin_username      = var.admin_user
+  admin_password      = var.admin_pass
+  network_interface_ids = [
+    azurerm_network_interface.int1.id,
+  ]
+    tags = {
+    environment = "prod"
+  }
 
-#  os_disk {
-#    caching              = "ReadWrite"
-#    storage_account_type = "Standard_LRS"
-#  }
-#
-#  source_image_reference {
-#    publisher = "MicrosoftWindowsServer"
-#    offer     = "WindowsServer"
-#    sku       = "2016-Datacenter"
-#    version   = "latest"
-#  }
-# }
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 
-# resource "azurerm_virtual_machine_extension" "AD" {
-#    name                    = "AD"
-#    virtual_machine_id      = azurerm_windows_virtual_machine.vm1.id
-#    publisher               = "Microsoft.Compute"
-#    type                    = "CustomScriptExtension"
-#    type_handler_version    = "1.10"
-#    
-#    settings = <<SETTINGS
-#    {
-#        "commandToExecute": "powershell.exe -Command \"${local.myad}\""
-#    }
-#    SETTINGS
-#}
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+ }
+
+ resource "azurerm_virtual_machine_extension" "AD" {
+    name                    = "AD"
+    virtual_machine_id      = azurerm_windows_virtual_machine.vm1.id
+    publisher               = "Microsoft.Compute"
+    type                    = "CustomScriptExtension"
+    type_handler_version    = "1.10"
+    
+    settings = <<SETTINGS
+    {
+        "commandToExecute": "powershell.exe -Command \"${local.myad}\""
+    }
+    SETTINGS
+}
